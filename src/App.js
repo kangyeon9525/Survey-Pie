27a441a18842143571e29f89735a6f4e52db42ac
kangyeon9525 <kangyeon9525@gmail.com>
2025-11-26 17:51:22 +0900

    feat : 설문조사 페이지 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import ProgressIndicator from './components/ProgressIndicator';
-import QuestionsBox from './components/QuestionBox';
-import PageA from './pages/pageA';
-import PageB from './pages/pageB';
+import CompletionPage from './pages/CompletionPage';
+import SurveyPage from './pages/SurveyPage';
 
 function App() {
-  const questions = [
-    {
-      title: '질문1',
-      desc: '설명1',
-      type: 'text',
-      required: false,
-      options: {},
-    },
-    {
-      title: '질문2',
-      desc: '설명2',
-      type: 'text',
-      required: true,
-      options: {},
-    },
-  ];
-
-  const step = 1;
-
-  const [answers, setAnswers] = useState([]);
-
   return (
     <div className="App">
-      {/*
-      <ProgressIndicator />
-      <QuestionsBox
-        question={questions[step]}
-        questionsLength={questions.length}
-        step={step}
-        answer={answers[step]}
-        setAnswer={(newAnswer) => {
-          setAnswers((answers) => {
-            const newAnswers = [...answers];
-            newAnswers[step] = newAnswer;
-            return newAnswers;
-          });
-        }}
-      />*/}
       <div>Hello World!</div>
       <Routes>
-        <Route path="/" element={<PageA />} />
-        <Route path="/2" element={<PageB />} />
+        <Route path="/done" element={<CompletionPage />} />
+        <Route path="/survey/:surveyId/" element={<SurveyPage />}>
+          <Route path=":step" element={<SurveyPage />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/pages/SurveyPage/index.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import ProgressIndicator from '../../components/ProgressIndicator';
+import QuestionsBox from '../../components/QuestionBox';
+
+function SurveyPage() {
+  const params = useParams();
+  console.log('params', params);
+
+  const questions = [
+    {
+      title: '질문1',
+      desc: '설명1',
+      type: 'text',
+      required: false,
+      options: {},
+    },
+    {
+      title: '질문2',
+      desc: '설명2',
+      type: 'text',
+      required: true,
+      options: {},
+    },
+  ];
+
+  const step = parseInt(params.step);
+
+  const [answers, setAnswers] = useState([]);
+
+  return (
+    <div>
+      <ProgressIndicator />
+      <QuestionsBox
+        question={questions[step]}
+        questionsLength={questions.length}
+        step={step}
+        answer={answers[step]}
+        setAnswer={(newAnswer) => {
+          setAnswers((answers) => {
+            const newAnswers = [...answers];
+            newAnswers[step] = newAnswer;
+            return newAnswers;
+          });
+        }}
+      />
+    </div>
+  );
+}
+
+export default SurveyPage;
